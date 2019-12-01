56e155674cc5a5ba8618e1c3299f98a4822fde23
Vincent Lee <vincent.lee@outlook.com>
2019-11-30 23:28:13 -0600

    Make compute_fp tail-call optimized

--- aoc_2019/day1.ml
+++ aoc_2019/day1.ml
@@ -3,11 +3,15 @@ open Core
 let compute x = (x / 3) - 2
 
 let compute_p2 x =
-  let rec compute_fp x =
-    if x <= 0 then 0
-    else x + compute_fp (compute x) (* TODO: Make this properly TCO-able? *)
+  let rec compute_fp acc x =
+    (* overshot and added x into acc when we shouldn'tve, so back up one
+       this could also be checked in the else, but whatever *)
+    if x <= 0 then acc - x 
+    else
+      let c = compute x in
+      compute_fp (acc + c) c
   in
-  compute_fp x - x (* Remove the initial mass *)
+  compute_fp 0 x
     
 let run () =
   let lines = Util.read_all_lines "d1_input.txt"
